3915bfd0999763edb252e992eedec7b12f19ce4d
Liulei <raymond.liu@tron.network>
2025-01-17 15:00:58 +0800

    WIP

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -8937,10 +8937,10 @@ impl AccountsDb {
         let mut slots = self.storage.all_slots();
         slots.sort_unstable();
         // todo 截断
-        slots.truncate(1000);
-        // if let Some(limit) = limit_load_slot_count_from_snapshot {
-        //     slots.truncate(limit); // get rid of the newer slots and keep just the older
-        // }
+        // slots.truncate(1000);
+        if let Some(limit) = limit_load_slot_count_from_snapshot {
+            slots.truncate(limit); // get rid of the newer slots and keep just the older
+        }
         let max_slot = slots.last().cloned().unwrap_or_default();
         let schedule = &genesis_config.epoch_schedule;
         let rent_collector = RentCollector::new(
