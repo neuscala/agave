3f3f165e085aef65ae041188b44c255642631934
Liulei <raymond.liu@tron.network>
2025-01-19 23:11:14 +0800

    WIP

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1564,17 +1564,19 @@ impl Bank {
         // Note that we are disabling the read cache while we populate the stakes cache.
         // The stakes accounts will not be expected to be loaded again.
         // If we populate the read cache with these loads, then we'll just soon have to evict these.
-        let (stakes, stakes_time) = measure!(Stakes::new(&fields.stakes, |pubkey| {
-            let (account, _slot) = bank_rc
-                .accounts
-                .load_with_fixed_root_do_not_populate_read_cache(&ancestors, pubkey)?;
-            Some(account)
-        })
-        .expect(
-            "Stakes cache is inconsistent with accounts-db. This can indicate \
-            a corrupted snapshot or bugs in cached accounts or accounts-db.",
-        ));
-        info!("Loading Stakes took: {stakes_time}");
+        // let (stakes, stakes_time) = measure!(Stakes::new(&fields.stakes, |pubkey| {
+        //     let (account, _slot) = bank_rc
+        //         .accounts
+        //         .load_with_fixed_root_do_not_populate_read_cache(&ancestors, pubkey)?;
+        //     Some(account)
+        // })
+        // .expect(
+        //     "Stakes cache is inconsistent with accounts-db. This can indicate \
+        //     a corrupted snapshot or bugs in cached accounts or accounts-db.",
+        // ));
+        // info!("Loading Stakes took: {stakes_time}");
+        // todo review
+        let stakes = Stakes::default();
         let stakes_accounts_load_duration = now.elapsed();
         let mut bank = Self {
             skipped_rewrites: Mutex::default(),
